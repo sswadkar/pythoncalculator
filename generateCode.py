14501d396badabdf38d48f84bfc050212566c6f9
Saraansh Wadkar <63820563+sswadkar@users.noreply.github.com>
2020-11-10 22:00:01 -0500

    Add files via upload

---
+++ generateCode.py
@@ -0,0 +1,35 @@
+import pynput
+from pynput.keyboard import Key, Controller
+import time
+keyboard = Controller()
+sign = ["+","-","*","/"]
+time.sleep(2)
+for x in range(43,100):
+    for y in range(100):
+        for z in range(len(sign)):
+            if sign[z] == "/":
+                if y != 0:
+                    ans = eval(str(x) + " " + sign[z] + " " + str(y))
+                    time.sleep(0.6)
+                    keyboard.type("if userinput1 == " + str(x) + " and sign == " + '"' + str(sign[z]) + '"' + " and userinput2 == " + str(y) +":")
+                    keyboard.press(Key.enter)
+                    keyboard.release(Key.enter)
+                    exp = str(x)+sign[z]+str(y) + "= " + str(ans)
+                    keyboard.type("print(" + '"' + str(exp) + '"' + ")")
+                    keyboard.press(Key.enter)
+                    keyboard.release(Key.enter)
+                    keyboard.press(Key.backspace)
+                    keyboard.release(Key.backspace)
+            else:
+                ans = eval(str(x) + " " + sign[z] + " " + str(y))
+                time.sleep(0.6)
+                keyboard.type("if userinput1 == " + str(x) + " and sign == " + '"' + str(sign[z]) + '"' + " and userinput2 == " + str(y) +":")
+                keyboard.press(Key.enter)
+                keyboard.release(Key.enter)
+                exp = str(x)+sign[z]+str(y) + "= " + str(ans)
+                keyboard.type("print(" + '"' + str(exp) + '"' + ")")
+                keyboard.press(Key.enter)
+                keyboard.release(Key.enter)
+                keyboard.press(Key.backspace)
+                keyboard.release(Key.backspace)
+
